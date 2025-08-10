66837407c4588c914b1b1b2e33bef7c4094311d5
maneira <sarahmaneira17@gmail.com>
2025-08-10 20:35:33 -0300

    execicio tabuada

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,14 @@ public class Main{
 
     public static void main(String[] args) {
           var scanner = new Scanner(System.in);
-//        System.out.println("Digite seu nome:");
-//        String name = scanner.next();
-//        System.out.println("Digite o ano que você nasceu:");
-//        int year = scanner.nextInt();
-//
-//        var result = 2025 - year;
-//        System.out.printf("Olá %s, você tem %s anos!", name, result);
+        System.out.println("Escolha um numero para saber a tabuada:");
+        int tabuada = scanner.nextInt();
 
-//        --------------------
+        for (var i=1; i<=10;i++) {
+            var result = i * tabuada;
+            System.out.println(i + "x"+tabuada + "=" + result);
 
-//        System.out.println("Digite o tamanho do lado do quadrado: ");
-//        int side = scanner.nextInt();
-//        var result = Math.pow(side, side);
-//        System.out.printf("%sm²", result);
-
-//        ---------------------
-
-//        System.out.println("Digite o tamanho do lado do retangulo: ");
-//        int width = scanner.nextInt();
-//        System.out.println("Digite a altura do retangulo: ");
-//        int heigth = scanner.nextInt();
-//        var result = width * heigth;
-//        System.out.printf("%sm²", result);
-
-//        ---------------------
-
-//        System.out.println("Digite seu nome:");
-//        var name1 = scanner.next();
-//        System.out.println("Digite sua idade:");
-//        var age1 = scanner.nextInt();
-//        System.out.println("Digite seu nome:");
-//        var name2 = scanner.next();
-//        System.out.println("Digite sua idade:");
-//        var age2 = scanner.nextInt();
-//        if (age1 > age2){
-//            System.out.printf("%s é %s anos mais velho que %s", name1, (age1 - age2), name2);
-//        } else {
-//            System.out.printf("%s é %s anos mais velho que %s", name2, (age2 - age1), name1);
-//        }
+        }
 
     }
 }
