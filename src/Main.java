d991fb96f5090abfdfdf3ba2541a3824cc0dfdc3
maneira <sarahmaneira17@gmail.com>
2025-08-10 01:19:20 -0300

    1.3 - Trabalhando com Operadores de Atribuição e Lógicos

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Main{
 
     public static void main(String[] args) {
-        byte number = 1;
-        short sNumber = 1;
-        int iNumber = 1;
-        long lNumber = 1L;
-        float fNumber = 1.1f;
-        char character = 'a';
-        String string = "a";
-        boolean bool = true;
-
+        var scanner = new Scanner(System.in);
+        System.out.println("Quantos anos você tem?");
+        var age = scanner.nextInt();
+        var canDrive = age >= 18;
+        System.out.printf("Você pode dirigir (%s)", canDrive);
     }
 }
