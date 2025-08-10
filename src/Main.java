154ddc6b7e3812b75252b5519ddbdfd9a6176839
maneira <sarahmaneira17@gmail.com>
2025-08-10 01:03:52 -0300

    Keywords e tipos primitivos

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main{
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite seu nome:");
-        String name = scanner.next();
-        System.out.println("Digite sua idade:");
-        int age = scanner.nextInt();
-        System.out.printf("Olá %s sua idade é %s", name, age);
+        byte number = 1;
+        short sNumber = 1;
+        int iNumber = 1;
+        long lNumber = 1L;
+        float fNumber = 1.1f;
+        char character = 'a';
+        String string = "a";
+        boolean bool = true;
+
     }
 }
