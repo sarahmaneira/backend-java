d951d0b07aa7e297a9b670e3fd1c4279985b3265
maneira <sarahmaneira17@gmail.com>
2025-08-11 20:46:29 -0300

    wip:crud

---
+++ src/br/com/dio/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.dio.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException (final String message){
+        super(message);
+
+    }
+
+}
