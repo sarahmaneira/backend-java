dae52612853510bcfa096fa7672dba938209eb77
sarahmaneira <sarahmaneira17@gmail.com>
2025-08-11 16:12:30 -0300

    wip: crud

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
-public class Main{
+import javax.management.RuntimeErrorException;
+
+public class Main {
 
     public static void main(String[] args) {
-          var scanner = new Scanner(System.in);
-        System.out.println("Escolha um numero para saber a tabuada:");
-        int tabuada = scanner.nextInt();
-
-        for (var i=1; i<=10;i++) {
-            var result = i * tabuada;
-            System.out.println(i + "x"+tabuada + "=" + result);
-
-        }
-
+        System.out.println("exception");   
     }
+
 }
---
+++ src/br/com/dio/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package br.com.dio.dao;
+
+import br.com.dio.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    
+    private List<UserModel> models = new ArrayList<>();
+    public
+}
